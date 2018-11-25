6d7d57e8603df4433f1e87aeb53a4a1a3395dcb7
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-11-25 06:22:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end 
   
   post "/teams" do 
-    @name = params[:name]
+    @name = params[:team][:name]
     @team = params[:team]
     @member1_name = params[:member1_name]
     @member1_power = params[:member1_power]
