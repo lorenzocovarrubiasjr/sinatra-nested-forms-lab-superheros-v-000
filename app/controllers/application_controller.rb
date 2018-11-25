e8752aa419462531ff872cdac3e965b1fa2db64e
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-11-25 06:37:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,16 +11,16 @@ class App < Sinatra::Base
   
   post "/teams" do 
     @name = params[:team][:name]
-    @team = params[:motto]
-    @member1_name = params[:member1_name]
-    @member1_power = params[:member1_power]
-    @member1_bio = params[:member1_bio]
-    @member2_name = params[:member2_name]
-    @member2_power = params[:member2_power]
-    @member2_bio = params[:member2_bio]
-    @member3_name = params[:member3_name]
-    @member3_power = params[:member3_power]
-    @member3_bio = params[:member3_bio]
+    @team = params[:team][:motto]
+    @member1_name = params[:team][:member1_name]
+    @member1_power = params[:team][:member1_power]
+    @member1_bio = params[:team][:member1_bio]
+    @member2_name = params[:team][:member2_name]
+    @member2_power = params[:team][:member2_power]
+    @member2_bio = params[:team][:member2_bio]
+    @member3_name = params[:team][:member3_name]
+    @member3_power = params[:team][:member3_power]
+    @member3_bio = params[:team][:member3_bio]
     
   end 
   
