4c95e7932f75655fe66753b215c9e1427370c831
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-11-25 07:00:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,9 @@ class App < Sinatra::Base
     @team_name = params[:team][:name]
     @team_motto = params[:team][:motto]
     @member1_name = params[:team][:members][][:name]
-    
+    @member1_name = params[:team][:members][][:power]
+    @member2_name = params[:team][:members][][:name]
+    @member3_name = params[:team][:members][][:name]
     
     
     erb :super_hero
